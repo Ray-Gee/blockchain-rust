db3813f60fea6f99fd4bf402f54428228f410436
UedaRyuichi <uedaryuichi.sales@gmail.com>
2021-09-26 05:55:17 +0900

    9/25

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main () {
-    println!("Hello, blockchain!");
+use blockchainlib::*;
+
+fn main() {
+    let block = Block::new(13, now(), vec![0; 32], 0, "Genesis block!".to_owned());
+
+    println!("{:?}", &block);
 }
